a5513344049f8218a01325ad4f118a686ddd4b59
saasbook <saasbook@saasbook.(none)>
2014-12-05 07:36:35 -0400

    view + model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,16 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+
+
+def self.all_ratings
+  Array['G','PG','PG-13','R','NC-17']
+end
+
+def Movie.get_all_ratings
+	all_ratings = []
+	self.select(:rating).group(:rating).each do |mo|
+		all_ratings << mo.rating
+	end
+	return all_ratings
+end
 end
